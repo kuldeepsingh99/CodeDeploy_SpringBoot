43ab26f1f21c2cad608711b162b15110e22713c8
Kuldeep Singh <kuldeepsingh99@gmail.com>
2020-01-26 00:51:24 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /opt/ec2-user/server
 rm -rf target/*
-mvn clean package
-mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=8080' > /dev/null 2> /dev/null < /dev/null &
+sudo mvn clean package
+sudo mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=8080' > /dev/null 2> /dev/null < /dev/null &
