36b37b431352750d6f19356ab815d4550fe96707
Kuldeep Singh <kuldeepsingh99@gmail.com>
2020-01-26 22:51:17 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd /opt/ubuntu/server
+cd /opt/root/server
 rm -rf target/*
 sudo mvn clean package
 sudo mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=8080' > /dev/null 2> /dev/null < /dev/null &
