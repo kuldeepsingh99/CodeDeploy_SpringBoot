7190eed2269bddc87ba3e8eb6104bdc6c103d1c0
Kuldeep Singh <kuldeepsingh99@gmail.com>
2020-02-01 11:10:48 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
-mvn clean package
-mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=8080' > /dev/null 2> /dev/null < /dev/null &
+sudo mvn clean package
+sudo mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=8080' > /dev/null 2> /dev/null < /dev/null &
