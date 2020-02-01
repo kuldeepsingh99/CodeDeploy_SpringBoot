00054f64b49bf9da6dbbd1788248c178b9289886
Kuldeep Singh <kuldeepsingh99@gmail.com>
2020-02-01 11:15:18 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,15 +3,15 @@
 # Kill Java process
 
 # Determine the pid
-echo "killing process starts"
-PID=`ps -C java -o pid=`
+#echo "killing process starts"
+#PID=`ps -C java -o pid=`
 
-if [ -n "$PID" ]; then
-    echo "killing process"
-    kill -9 $PID
-else
-    echo "empty"
-fi
+#if [ -n "$PID" ]; then
+#    echo "killing process"
+#    kill -9 $PID
+#else
+#    echo "empty"
+#fi
 
 cd /opt
 rm -rf root
